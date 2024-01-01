8650d60d858128992b6ab2f54931861beac7bb84
Jenny <14525577+jennyd20@users.noreply.github.com>
2024-01-01 13:42:05 -0800

    Finish Day11 part 2, simplify ALL THE CODE

--- 2023/day11.py
+++ 2023/day11.py
@@ -2,88 +2,66 @@ import lib
 
 
 def find_lengths(space_image):
-    print_image(space_image)
-    expanded_img = expand_space(space_image)
-    galaxies = find_galaxies(expanded_img)
+    empty_rows_and_cols, galaxy_list = process_space(space_image)
 
     # Now find the lengths between all galaxy pairs
-    print_image(expanded_img)
+    if part1:
+        expansion_multiplier = 2
+    else:
+        expansion_multiplier = 1000000
+
     sum_shortest_paths = 0
-    while galaxies:
-        from_gal = galaxies.pop()
-        for to_gal in galaxies:
-            xdif = abs(from_gal[0] - to_gal[0])
-            ydif = abs(from_gal[1] - to_gal[1])
-            # print(f"compare {from_gal[2]} to {to_gal[2]}")
-            # print(f"xdif: {xdif}, ydif: {ydif}")
-            # print(f"total diff: {xdif + ydif}")
-            # print()
-            sum_shortest_paths += xdif + ydif
+
+    while galaxy_list:
+        from_gal = galaxy_list.pop()
+        for to_gal in galaxy_list:
+            # Determine how many empty rows/cols are crossed between the paths
+            # index 1 is row, index 2 is col.  Check both instead of duplicating code
+            total_expansions = 0
+            axis_diff = 0
+            for i in range(2):
+                from_gal_i = from_gal[i]
+                to_gal_i = to_gal[i]
+                axis_diff += abs(from_gal_i - to_gal_i)
+                for x in empty_rows_and_cols[i]:
+                    if min(from_gal_i, to_gal_i) < x < max(from_gal_i, to_gal_i):
+                        total_expansions += 1
+
+            sum_shortest_paths += (
+                axis_diff + (total_expansions * (expansion_multiplier - 1))
+            )
 
     return sum_shortest_paths
 
 
-def expand_space(original_space):
-    # For part 1, we're only expanding x2
-    expansion_value = 5
-    if part1:
-        expansion_value = 2
-    print(f"{expansion_value=}")
+def process_space(original_space):
+    # Just return a list of rows and a list of columns that are empty and will be expanded
 
-    # Check every row and column at the same time
-    expanded_space = []
+    empty_rows = set()
+
     # Create a list of all columns, then eliminate cols that have galaxies in them.
     # At the end of the loop, all that's remaining will be the indices of empty columns
-    empty_cols = [i for i in range(len(original_space[0]))]
     # Making the assumption that space is square
+    empty_cols = set(range(len(original_space[0])))
+
+    galaxy_list = []
 
     for x, row in enumerate(original_space):
-        expanded_space.append(row)
-        # First, just check the whole line to see if the row is empty.
-        # If so, add an extra empty row
-        if row.find("#") < 0:
-            for i in range(expansion_value - 1):
-                expanded_space.append("." * len(row))
-        for y in range(len(row)):
+        row_is_empty = True
+        for y, point in enumerate(row):
             if row[y] == "#":
+                galaxy_list.append([x, y])
+                row_is_empty = False
                 if y in empty_cols:
                     empty_cols.remove(y)
+        if row_is_empty:
+            empty_rows.add(x)
 
-    # Now that we have tracked the empty columns, add them to every line in the list
-    for x, row in enumerate(expanded_space):
-        # Go through the list backwards to adding to empty columns so the addition doesn't mess up the stuff behind
-        for col_to_add in reversed(empty_cols):
-            row = row[:col_to_add] + "." * (expansion_value - 1) + row[col_to_add:]
-        expanded_space[x] = row
-    return expanded_space
-
-
-def find_galaxies(image):
-    # return {(x1, y1), (x2, y2), ... }
-    galaxies = []
-    for x, row in enumerate(image):
-        for y in range(len(row)):
-            if row[y] == "#":
-                # Start counting from 1 instead of 0 to match example labeling of galaxies
-                galaxy_num = len(galaxies) + 1
-
-                # Messes things up for going through columns, but good for debugging
-                # replaced_row_galaxy = row[:y] + str(galaxy_num) + row[y + 1 :]
-                # image[x] = replaced_row_galaxy
-                # row = replaced_row_galaxy
-                galaxies.append([x, y, galaxy_num])
-    print(f"Number of galaxies: {len(galaxies)}")
-    return galaxies
-
-
-def print_image(image):
-    for i in image:
-        print(i)
-    print()
+    return [empty_rows, empty_cols], galaxy_list
 
 
 ### SCRIPT ARGUMENTS AND GLOBAL VARIABLES ###
-use_example = True
+use_example = False
 part1 = False
 alt_file = None
 # alt_file = "day11_ex2.txt"
@@ -93,31 +71,3 @@ if __name__ == "__main__":
     input_text = lib.read_input(__file__, use_example, alt_file)
     answer = find_lengths(input_text.splitlines())
     print(answer)
-
-"""
-For part 2:
-
-Trying to run programmatically will cause the CPU to explode
-
-Expanding empty space x1,000,000 instead of x2
-
-number of pairings = n*(n+1) / 2
-
-In example:
-9 total galaxies
-36 pairs
-
-x1 = 292
-x2 = 374
-x3 = 456
-x4 = 538
-x5 = 620
-x10 = 1030 
-x100 = 8410
-
-
-My input:
-443 galaxies
-98346 pairs
-
-"""
