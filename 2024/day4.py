fb98605d35244d6b5b00928cd559dc5a945dbc22
Jenny <14525577+jennyd20@users.noreply.github.com>
2025-01-14 13:59:40 -0800

    Update day4 to use new Grid libraries

--- 2024/day4.py
+++ 2024/day4.py
@@ -1,115 +1,81 @@
-from re import L
-
-
-DIR = {
-    "n": (-1, 0),
-    "s": (1, 0),
-    "w": (0, -1),
-    "e": (0, 1),
-    "ne": (-1, 1),
-    "se": (1, 1),
-    "nw": (-1, -1),
-    "sw": (1, -1),
-}
-
-
-class Grid:
-    def __init__(self, input):
-        self.rows = input.splitlines()
-        self.max_rows = len(self.rows)
-        self.max_cols = len(self.rows[0])
-
-    def get_val(self, row, col):
-        return self.rows[row][col]
+from aoc_libs import grid as grid_lib
 
 
 def process_input(input):
-    # For every character in the grid, check to see if there's the word search XMAS in all 8 directions
-    grid = Grid(input)
+    grid = grid_lib.Grid(input)
 
     word_count = 0
 
-    for row in range(grid.max_rows):
-        for col in range(grid.max_cols):
-            if part1:
-                # Only start searching the grid if the first letter is correct
-                if grid.get_val(row, col) == SEARCH_WORD[0]:
-                    #print(f"\nFirst letter matches at {row=}, {col=}")
-                    for dir in DIR:
-                        if check_for_word(grid, row, col, dir):
-                            word_count += 1
-
-            # Part2, MAS in the shape of an X
-            else:
-                # The middle letter that we'll search from is always an A
-                if grid.get_val(row, col) == "A":
-                    if check_for_x(grid, row, col):
+    for pos in grid.all_pos():
+        # For every character in the grid, check to see if there's the word search XMAS in all 8 directions
+        if part1:
+            # Only start searching the grid if the first letter is correct
+            if grid.get_val(pos) == SEARCH_WORD[0]:
+                # print(f"\nFirst letter matches at {pos=}")
+                for dir in grid.dir_list():
+                    if check_for_word(grid, pos, dir):
                         word_count += 1
+
+        # Part2, MAS in the shape of an X
+        else:
+            # The middle letter that we'll search from is always an A
+            if grid.get_val(pos) == "A":
+                if check_for_x(grid, pos):
+                    word_count += 1
     return word_count
 
 
-def check_for_word(grid, i, j, dir):
-
+def check_for_word(grid, start_pos, dir):
     # Verified the first letter already, so start traveling along dir to start
-    i, j = update_row_col(i, j, dir)
+    pos = start_pos.duplicate()
     for c in SEARCH_WORD[1:]:
-        if not coords_in_bounds(grid, i, j):
-            #print(f"Out of bounds at {i=}, {j=}")
+        # print(f"Checking for {c}")
+        pos.go_dir(dir)
+        if pos.out_of_bounds(grid):
+            # print(f"Out of bounds at {pos=}")
             return False
-        char = grid.get_val(i, j)
+        char = grid.get_val(pos)
+        # print(f"Found {char=} at {pos=}")
         if char != c:
-            #print(f"No match in {dir=}: {c=} != {char=} at {i=}, {j=}")
+            # print(f"No match in {dir=}: {c=} != {char=} at {pos=}")
             return False
-        else:
-            i, j = update_row_col(i, j, dir)
-    #print(f"Success!! {dir=}")
+    # print(f"Success!! {dir=}")
     return True
 
 
-def check_for_x(grid, row, col):
-    #print(f"\nChecking at center {row=}, {col=}")
+def check_for_x(grid, start_pos):
+    # print(f"\nChecking at center {row=}, {col=}")
     # Each pair of directions has to be either M or S, but not two of one: (nw, se) and (ne, sw)
-    diag_lines = (("nw", "se"), ("ne", "sw"))
 
-    for diag_pair in diag_lines:
-        i = None
-        j = None
+    for diag_pair in grid.get_diag_pairs():
         used_m = False
         used_s = False
-        for p in diag_pair:
-            i, j = update_row_col(row, col, p)
-            if not coords_in_bounds(grid, i, j):
+        for diag_dir in diag_pair:
+            pos = start_pos.duplicate().go_dir(diag_dir)
+            if pos.out_of_bounds(grid):
                 return False
-            #print(f"Checking corner in dir {p}")
-            char = grid.get_val(i, j)
-            print(f"{char=}")
+            # print(f"Checking corner in dir {p}")
+            char = grid.get_val(pos)
+            # print(f"{char=}")
             used_m = used_m or char == "M"
             used_s = used_s or char == "S"
         if not (used_m and used_s):
-            #print("No go")
+            # print("No go")
             return False
 
-    #print("X found")
+    # print("X found")
     return True
 
 
-def update_row_col(row, col, dir):
-    return row + DIR[dir][0], col + DIR[dir][1]
-
-
-def coords_in_bounds(grid, row, col):
-    return 0 <= row < grid.max_rows and 0 <= col < grid.max_cols
-
-
 ########### SCRIPT ARGUMENTS AND GLOBAL VARIABLES ###########
-import lib
-
 SEARCH_WORD = "XMAS"
 part1 = False
+use_example = True
 
 # Execute the script
+from aoc_libs import lib
+
 if __name__ == "__main__":
-    use_example = False
     input_text = lib.read_input(__file__, use_example)
 
     answer = process_input(input_text)
